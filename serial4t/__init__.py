f1552901dbf64d8654ee7f434abe3033a8e5b8b0
Wen-Hung , Chang <bearada1975@gmail.com>
2021-03-13 08:23:17 +0800

    auto detect port

--- serial4t/__init__.py
+++ serial4t/__init__.py
@@ -1,10 +1,26 @@
 import serial
-
+import serial.tools.list_ports
+import os.path
+import platform
 
 __all__ = [ 
             '序列連接', '序列連接microbit'
             
             ]
+
+def detect_potential_ports():
+    # code from list_serial_ports 
+    
+    try:
+        old_islink = os.path.islink
+        if platform.system() == "Windows":
+            os.path.islink = lambda _: False
+        all_ports = list(serial.tools.list_ports.comports())
+    finally:
+        os.path.islink = old_islink
+
+    return [(p.device, p.description) for p in all_ports 
+                    if (p.vid, p.pid) in {(0x0D28, 0x0204)} ]
 
 
 def 序列連接(連接埠, 傳輸率=115200, timeout=None):
@@ -12,8 +28,24 @@ def 序列連接(連接埠, 傳輸率=115200, timeout=None):
 
 
 def 序列連接microbit(連接埠='auto', 傳輸率=115200, timeout=None):
-    pass
+    print('connect microbit')
+    if 連接埠 == 'auto':
+        potential = detect_potential_ports()
+        #print(potential)
+        microbit_num = len(potential)
+        if  microbit_num == 1:
+            port = potential[0][0]
+            print('port: ', port)
+        elif microbit_num > 1:
+            _, descriptions = zip(*potential)
 
+            message = f'偵測到{microbit_num}片microbit(如下)。使用自動連接時，請只使用1片'
+            message += "\n * " + "\n * ".join(descriptions)
+            print(message)
+            return
+        else :
+            print('偵測不到microbit，請確認是否已接上電腦')
+            return
 
 class Connection():
     def __init__(self, ser):
