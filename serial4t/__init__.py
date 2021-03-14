429fc7fa076fe094f07cc4ed8b09c1e19941f776
Wen-Hung , Chang <bearada1975@gmail.com>
2021-03-14 09:43:47 +0800

    basic structure done

--- serial4t/__init__.py
+++ serial4t/__init__.py
@@ -8,6 +8,13 @@ __all__ = [
             
             ]
 
+
+## Custom Exceptions
+class SecralConnectionError(Exception):
+    def __init__(self, value):
+        super().__init__(value)
+
+
 def detect_potential_ports():
     # code from list_serial_ports 
     
@@ -23,53 +30,172 @@ def detect_potential_ports():
                     if (p.vid, p.pid) in {(0x0D28, 0x0204)} ]
 
 
-def 序列連接(連接埠, 傳輸率=115200, timeout=None):
+
+
+
+
+def 序列連接(埠, 傳輸率=115200, timeout=None):
     pass
 
 
-def 序列連接microbit(連接埠='auto', 傳輸率=115200, timeout=None):
-    print('connect microbit')
-    if 連接埠 == 'auto':
+def 序列連接microbit(埠='auto', 例外錯誤=True ,傳輸率=115200, 讀取等待=None):
+    if 埠 == 'auto':
+        #print('<< 自動偵測microbit.... >>')
         potential = detect_potential_ports()
         #print(potential)
         microbit_num = len(potential)
         if  microbit_num == 1:
             port = potential[0][0]
-            print('port: ', port)
+            print(f'<< 偵測到microbit, 在連接埠 {port} >> ' )
         elif microbit_num > 1:
             _, descriptions = zip(*potential)
-
-            message = f'偵測到{microbit_num}片microbit(如下)。使用自動連接時，請只使用1片'
+            message = f'<< 偵測到{microbit_num}個microbit, 相關資訊如下 >>'
             message += "\n * " + "\n * ".join(descriptions)
             print(message)
-            return
+            port = potential[0][0]
+            print(f'<< 將使用連接埠({port}) >>')
         else :
-            print('偵測不到microbit，請確認是否已接上電腦')
-            return
+            msg = '<< 偵測不到microbit, 請確認是否已接上電腦 >>'
+            if 例外錯誤 :
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return Connection(enable_exception=例外錯誤)
+
+    else:
+        port = 埠
+
+    return Connection(port, 例外錯誤, 傳輸率, 讀取等待)
+
 
 class Connection():
-    def __init__(self, ser):
-        self.ser = ser
+    def __init__(self, port=None, enable_exception=True, baudrate=115200, timeout=None):
+        self.port = port
+        self.enable_exception = enable_exception
+        self.baudrate = baudrate
+        self.timeout = timeout
+
+        if port :
+            # serial connect
+            try:
+                self.ser = serial.Serial(port, baudrate, timeout=timeout)
+                print('<< microbit已連接 ({}) >>'.format(port))
+            except serial.SerialException:
+                msg = '<< 連接microbit時發生錯誤 >>'
+                if self.enable_exception:
+                    raise SecralConnectionError(msg)
+                else:
+                    print(msg)
+        else:
+            # no port
+            self.ser = None
 
     def 傳送位元組(self, send_bytes):
-        pass
+        if not self.ser:
+            msg = '<< 沒有連接microbit >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
 
-    def 傳送文字(self, send_text):
-        pass
+        try:
+            if type(send_bytes) is not bytes:
+                send_bytes = bytes(str(send_bytes), 'utf8')
+            self.ser.write(send_bytes)
 
-    def 接送位元組(self, bytes_num=None):
-        pass
+        except serial.SerialException:
+            msg = '<< 傳送microbit時發生錯誤 >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return       
+
+
+    # def 傳送文字(self, send_text):
+    #     if not self.ser:
+    #         msg = '<< 沒有連接microbit >>'
+    #         if self.enable_exception:
+    #             raise SecralConnectionError(msg)
+    #         else:
+    #             print(msg)
+    #             return
+
+    def 接收位元組(self, 位元組=1):
+        if not self.ser:
+            msg = '<< 沒有連接microbit >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
+
+        try:
+            if self.timeout != 0:
+                print('<< 等待讀取microbit... >>')
+
+            return self.ser.read(size=位元組)
+
+        except serial.SerialException:
+            msg = '<< 讀取microbit時發生錯誤 >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
+
+    # def 接收文字(self):
+    #     if not self.ser:
+    #         msg = '<< 沒有連接microbit >>'
+    #         if self.enable_exception:
+    #             raise SecralConnectionError(msg)
+    #         else:
+    #             print(msg)
+    #             return
 
     def 關閉(self):
-        pass
+        if not self.ser:
+            msg = '<< 沒有連接microbit >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
+
+        try:
+            self.ser.close()
+            print('<< 序列連線已關閉 >>')
+            self.ser = None
+        except serial.SerialException:
+            msg = '<< 關閉microbit時發生錯誤 >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
 
 
+    def 清除緩除區(self):
+        if not self.ser:
+            msg = '<< 沒有連接microbit >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
 
-### Custom Exceptions
-# class ImageReadError(Exception):
-#     def __init__(self, value):
-#         message = f"無法讀取圖片檔 (檔名:{value})"
-#         super().__init__(message)
+        try:
+            self.ser.reset_input_buffer()        
+        except serial.SerialException:
+            msg = '<< 清除緩除區時發生錯誤 >>'
+            if self.enable_exception:
+                raise SecralConnectionError(msg)
+            else:
+                print(msg)
+                return
+
+
 
 ### wrapper functions
 
